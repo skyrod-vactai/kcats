f8345d5ba28e33e4465652398f79a4c9ef590102
Skyrod Vactai <skyrod@skyrod.me>
2024-11-10 09:09:20 -0500

    Allow emoji characters in our flavor of EDN

--- edn-format/src/lib.rs
+++ edn-format/src/lib.rs
@@ -456,6 +456,22 @@ fn is_whitespace(c: char) -> bool {
     c.is_whitespace() || c == ','
 }
 
+fn is_emoji(c: char) -> bool {
+    matches!(c,
+        '\u{1F300}'..='\u{1F5FF}' |   // Misc Symbols and Pictographs
+        '\u{1F600}'..='\u{1F64F}' |   // Emoticons
+        '\u{1F680}'..='\u{1F6FF}' |   // Transport and Map
+        '\u{1F700}'..='\u{1F77F}' |   // Alchemical Symbols
+        '\u{1F780}'..='\u{1F7FF}' |   // Geometric Shapes Extended
+        '\u{1F800}'..='\u{1F8FF}' |   // Supplemental Arrows-C
+        '\u{1F900}'..='\u{1F9FF}' |   // Supplemental Symbols and Pictographs
+        '\u{2600}'..='\u{26FF}' |     // Miscellaneous Symbols
+        '\u{2700}'..='\u{27BF}' |     // Dingbats
+        '\u{FE00}'..='\u{FE0F}' |     // Variation Selectors
+        '\u{1FA70}'..='\u{1FAFF}'     // Symbols and Pictographs Extended-A
+    )
+}
+
 fn is_allowed_atom_character(c: char) -> bool {
     c == '.'
         || c == '*'
@@ -474,6 +490,7 @@ fn is_allowed_atom_character(c: char) -> bool {
         || c == ':'
         || c.is_alphabetic()
         || c.is_numeric()
+        || is_emoji(c)
 }
 
 fn equal(v1: &Value, v2: &Value) -> bool {
